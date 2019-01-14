d5b32ae3c2aa2e142e6b4744b25cf6138242faf9
wuyaowang <wuyaowang@atguigu.com>
2019-01-14 19:44:04 +0800

    第一次连接

---
+++ src/test/java/web04/helloTest.java
@@ -0,0 +1,14 @@
+package web04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class helloTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
