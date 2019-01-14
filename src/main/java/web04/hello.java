bf85c7f68d310ce6703c1aa210c2efe140c363ee
wuyaowang <wuyaowang@atguigu.com>
2019-01-14 20:56:07 +0800

    第三次提交too

--- src/main/java/web04/hello.java
+++ src/main/java/web04/hello.java
@@ -4,5 +4,13 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("222");
+		System.out.println("222");
+		System.out.println("222");
+		System.out.println("222");
+		System.out.println("222");
+		
+		System.out.println("222");
+		
+		System.out.println("222");
 	}
 }
